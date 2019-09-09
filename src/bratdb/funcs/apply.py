22c5c3999efc0c93392dd4467e002bebd18783e4
dcronkite <dcronkite@gmail.com>
2019-09-09 13:56:37 -0700

    Build application portion

--- src/bratdb/funcs/apply.py
+++ src/bratdb/funcs/apply.py
@@ -1,11 +1,57 @@
 import datetime
+import os
+import re
 
 from loguru import logger
 
 from bratdb.funcs.utils import get_output_path
 
 
-def apply_regex_to_corpus(regex, outpath=None, **kwargs):
+def get_documents(directory=None, connection_string=None, query=None,
+                  encoding='utf8', **kwargs):
+    if directory:
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                name = file.split('.')[0]
+                fp = os.path.join(root, file)
+                with open(fp, encoding=encoding, errors='ignore') as fh:
+                    text = clean_text(fh.read().lower())
+                yield name, text
+
+
+def clean_text(text):
+    return text.replace('\n', ' ').replace('\t', ' ')
+
+
+def compile_regexes(regex_file, encoding='utf8'):
+    """
+    Reads and compiles regular expressions
+    :param encoding:
+    :param regex_file: concept \t term \t regex
+    :return:
+    """
+    res = []
+    with open(regex_file, encoding=encoding) as fh:
+        for line in fh:
+            concept, term, regex = line.strip().split('\t')
+            res.append((concept, term, re.compile(regex)))
+    return res
+
+
+def apply_regex_to_corpus(regex, outpath=None, encoding='utf8', **kwargs):
     _outpath = get_output_path(regex, outpath, exts=('apply',))
     dt = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-    outpath = f'{outpath}.{dt}.tsv'
+    outpath = f'{_outpath}.{dt}.tsv'
+    logger.info(f'Primary output file: {outpath}')
+    regexes = compile_regexes(regex, encoding)
+    logger.info(f'Compiled {len(regexes)} regexes.')
+    rx_cnt = 0
+    with open(outpath, 'w') as out:
+        out.write('document\tconcept\tcaptured\n')
+        for i, (name, doc) in enumerate(get_documents(**kwargs)):
+            for concept, term, regex in regexes:
+                for m in regex.finditer(doc):
+                    rx_cnt += 1
+                    out.write(f'{name}\t{concept}\t{term}\t{m.group()}\n')
+        if i % 1000 == 0:
+            logger.info(f'Completed {i + 1} documents ({rx_cnt} concepts identified)')
--- src/bratdb/scripts/apply.py
+++ src/bratdb/scripts/apply.py
@@ -9,10 +9,16 @@ def main():
     parser.add_argument('regex',
                         help='Regex tsv file of bdb-extract-regex process;'
                              ' concept {tab} term {tab} regex')
+    parser.add_argument('--directory', default=None,
+                        help='Directory containing files to process')
+    parser.add_argument('--connection-string', default=None, dest='connection_string',
+                        help='sqlalchemy-flavored connection string')
     parser.add_argument('--outpath', default=None,
                         help='Output file containing frequencies')
     parser.add_argument('--logdir', default='.',
                         help='Directory to place log files.')
+    parser.add_argument('--query', default=None, nargs='+',
+                        help='query to retrieve name, document pairs from database table')
     args = parser.parse_args()
     initialize_logging(logdir=args.logdir)
     apply_regex_to_corpus(**vars(args))
