409224d81c70e9632778b1e58b77945072d370b3
dcronkite <dcronkite@gmail.com>
2019-09-09 12:45:27 -0700

    Added regexify function as well as a very dumb stemmer

--- src/bratdb/funcs/regexify.py
+++ src/bratdb/funcs/regexify.py
@@ -1,7 +1,46 @@
+import re
+
 from loguru import logger
 
 from bratdb.funcs.utils import get_output_path
+from bratdb.nlp.stemmer import Stemmer
 
 
-def regexify_keywords_to_file(extract, outpath=None):
+def regexify_keywords_to_file(extract, outpath=None, encoding='utf8',
+                              extra_slop=1, **kwargs):
     _outpath = get_output_path(extract, outpath, exts=('regexify',))
+    outpath = f'{_outpath}.tsv'
+    code_pat = re.compile(r'\d+\.\d+')
+    num_pat = re.compile(r'\d+')
+    slop_pat = re.compile(r'\[W(?P<slop>\d+)<\|(?P<punct>.*?)\|>\]')
+    regexes = []
+    with open(extract, encoding=encoding) as fh:
+        for line in fh:
+            concept, keywords, term = line.strip().split('\t')
+            words = []
+            prev_word = False
+            for word in term.split(' '):
+                if code_pat.match(word):
+                    regexes.append((concept, keywords, fr'\b{word}\b'))
+                elif num_pat.match(word):
+                    if prev_word:
+                        words.append(r'\W*')
+                    words.append(r'\d+')
+                    prev_word = True
+                elif slop_pat.match(word):
+                    m = slop_pat.match(word)
+                    cnt = int(m.group('slop')) + extra_slop
+                    if '.' in m.group('punct') or ';' in m.group('punct'):
+                        words.append(rf'\W*(\w+\W*){{0,{cnt}}}')
+                    else:
+                        words.append(rf'[^\w\.;]*(\w+[^\w\.;]*){{0,{cnt}}}')
+                    prev_word = False
+                else:  # is word
+                    if prev_word:
+                        words.append(r'\W*')
+                    words.append(Stemmer.transform(word))
+                    prev_word = True
+            regexes.append((concept, keywords, ''.join(words)))
+
+    with open(outpath, 'w', encoding=encoding) as out:
+        out.write('\n'.join('\t'.join(line) for line in regexes))
---
+++ src/bratdb/nlp/stemmer.py
@@ -0,0 +1,39 @@
+class Stemmer:
+    """
+    Stemmer which provides regular expression variations
+    """
+
+    @staticmethod
+    def replace(word, suffix, repl):
+        res = word.rfind(suffix)
+        return word[:res] + repl
+
+    @staticmethod
+    def transform(word):
+        if word.endswith('sses'):
+            word = Stemmer.replace(word, 'sses', 'ss(es|ing|ed)?')
+        elif word.endswith('ies'):
+            word = Stemmer.replace(word, 'ies', '(y|ies|ying|ied)?')
+        elif word.endswith('ss'):
+            word = Stemmer.replace(word, 'ss', 'ss(es|ing|ed)?')
+        elif word.endswith('ch'):
+            word = Stemmer.replace(word, 'ch', 'ch(es|ing|ed)?')
+        elif word.endswith('s'):
+            word = Stemmer.replace(word, 's', r'(s|\w?ing|\w?ed)?')
+        elif word.endswith('eed'):
+            word = Stemmer.replace(word, 'eed', r'ee(s|ing|d)?')
+        elif word.endswith('ied'):
+            word = Stemmer.replace(word, 'ied', '(y|ys|ies|ying|i?ed)?')
+        elif word.endswith('ed'):
+            word = Stemmer.replace(word, 'ed', r'?e?(s|ing|d)?')
+        elif word.endswith('ying'):
+            word = Stemmer.replace(word, 'y', '(y|ys|ies|ying|i?ed)?')
+        elif word.endswith('ing'):
+            word = Stemmer.replace(word, 'ing', r'?e?(s|ing|d)?')
+        elif word.endswith('y'):
+            word = Stemmer.replace(word, 'y', '(y|ys|ies|ying|i?ed)?')
+        elif word.endswith('al'):
+            word = Stemmer.replace(word, 'al', '(al)?e?(s|ing|d)?')
+        else:
+            word += '?(s|ing|ed)'
+        return word
