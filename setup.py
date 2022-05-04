69aa3f5099250a5cc0027e3580ab4fce03b98530
dcronkite <dcronkite@gmail.com>
2022-05-04 13:39:53 -0700

    Added option to include context of regular expression as debugging output.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 import setuptools
 
 setup(name='bratdb',
-      version='0.0.7',
+      version='0.1.0',
       description='Extract brat-formatted data to prepare the annotated data for various NLP applications',
       url='https://github.com/kpwhri/bratdb',
       author='dcronkite',
       author_email='dcronkite@gmail.com',
       license='MIT',
       classifiers=[  # from https://pypi.python.org/pypi?%3Aaction=list_classifiers
-          'Development Status :: 3 - Alpha',
+          'Development Status :: 4 - Beta',
           'Intended Audience :: Science/Research',
           'Programming Language :: Python :: 3 :: Only',
           'Topic :: Text Processing :: Linguistic',
--- src/bratdb/funcs/apply.py
+++ src/bratdb/funcs/apply.py
@@ -59,20 +59,55 @@ def check_time_expired(start_time, run_hours):
     return start_time + datetime.timedelta(hours=run_hours) > datetime.datetime.now()
 
 
-def apply_regexes_to_text(regexes, text, newline_replace=' ', exclude_captured=False):
+def apply_regexes_to_text(regexes, text, newline_replace=' ',
+                          exclude_captured=False, include_context: int = 0):
     for concept, term, regex in regexes:
         for m in regex.finditer(text):
             capture = '' if exclude_captured else m.group()
             capture = capture.replace('\n', newline_replace)
-            yield concept, term, capture
+            if include_context:
+                precontext = text[max(0, m.start() - include_context):m.start()].strip().replace('\n', newline_replace)
+                postcontext = text[m.end():m.end() + include_context].strip().replace('\n', newline_replace)
+                yield concept, term, capture, precontext, postcontext
+            else:
+                yield concept, term, capture
 
 
-def apply_regex_to_df(regex, df, *, encoding='utf8', newline_replace=' ', exclude_captured=False):
+def apply_regex_to_df(regex, df, *, encoding='utf8', newline_replace=' ',
+                      exclude_captured=False, include_context: int = 0):
+    """Apply regular expressions to a dataframe rather than text.
+
+    :param regex: file containing regular expressions in bratdb format
+    :param df:
+    :param encoding:
+    :param newline_replace:
+    :param exclude_captured:
+    :param include_context: specify the amount of contextual characters to include.
+
+    :return: list of tuples that can be loaded into pandas dataframe with
+        pd.DataFrame(
+            apply_regex_to_df(regex_file, df),
+            columns=['id', 'concept', 'term', 'capture']
+        )
+        or
+        pd.DataFrame(
+            apply_regex_to_df(regex_file, df, include_context=100),
+            columns=['id', 'concept', 'term', 'capture', 'precontext', 'postcontext']
+        )
+    """
     regexes = compile_regexes(regex, encoding)
     res = []
     for r in df.itertuples():
-        for concept, term, capture in apply_regexes_to_text(regexes, r.note_text):
-            res.append((r.Index, concept, term, capture))
+        for concept, term, capture, *rest in apply_regexes_to_text(
+                regexes, r.note_text,
+                include_context=include_context,
+                newline_replace=newline_replace,
+                exclude_captured=exclude_captured,
+        ):
+            if include_context:
+                res.append((r.Index, concept, term, capture, rest[0], rest[1]))
+            else:
+                res.append((r.Index, concept, term, capture))
     return res
 
 
@@ -81,7 +116,6 @@ def apply_regex_to_corpus(regex, outpath=None, encoding='utf8',
                           log_incr=10000, newline_replace=' ',
                           **kwargs):
     """
-
     :param regex:
     :param outpath:
     :param encoding:
